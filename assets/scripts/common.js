7c30204483dd4dab3761b0054ea5b4740af19d24
Vasinc <lein0.jade@gmail.com>
2022-05-07 15:31:03 +0300

    8 (all pages added - no content - responsive)

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -1,27 +1,23 @@
+const burgerMenu = document.querySelector('.burger-menu');
 const nav = document.querySelector('nav');
-const ul = document.querySelector('ul');
-const links = nav.querySelectorAll('a');
-const home = document.getElementById('home');
-
-console.log(links)
+const backdrop = document.querySelector('.backdrop');
 
 
-window.addEventListener('scroll', () => {
-    if(window.pageYOffset > nav.clientHeight && window.innerWidth > 800) {
-        nav.style.position = 'fixed';
-        nav.style.background = `orange`;
-        links.forEach(link => {
-            link.style.color = 'white';
-        })
-        home.style.float = 'left';
-        ul.style.float = 'right';
-    } else {
-        nav.style.position = 'initial';
-        nav.style.background = `white`;
-        links.forEach(link => {
-            link.style.color = 'black';
-        })
-        home.style.float = 'none';
-        ul.style.float = 'none';
+function toggleMenu() {
+    nav.classList.toggle('display-flex');
+    backdrop.classList.toggle('display-block');
+    document.body.classList.toggle('overflow-hidden');
+}
+
+burgerMenu.addEventListener('click', () => {
+    toggleMenu();
+    nav.scrollIntoView();
+});
+backdrop.addEventListener('click', toggleMenu);
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 800 && backdrop.classList.contains('display-block')) {
+        toggleMenu();
     }
 })
+
