e4c67d044a3f98c35c420ee24ce52086b11206c4
Vasinc <lein0.jade@gmail.com>
2022-06-18 23:53:33 +0300

    19 ( slots movement done )

--- assets/scripts/gamble.js
+++ assets/scripts/gamble.js
@@ -6,12 +6,35 @@ const COLORS = ['red', 'blue', 'cyan', 'green', 'pink', 'magenta', 'purple', 'ye
 let isSpinning = false;
 let randomSpins;
 let auto_slots;
+let prvColors = [0, 0, 0];
+let prvColors1 = [0, 0, 0];
+let prvColors2 = [0, 0, 0];
 
 
 function changeSlots () {
-    for(let i=0; i<COLORS.length; i++) {
-        const rndNum = Math.trunc(Math.random() * COLORS.length);
-        slots[i].style.background = `${COLORS[rndNum]}`;
+    for(let i=0; i<3; i++) {
+        if (i == 0) {
+            rndNum = Math.trunc(Math.random() * COLORS.length);
+            prvColors.unshift(rndNum);
+            prvColors.pop;
+            slots[i].style.background = COLORS[prvColors[0]];
+            slots[i + 3].style.background = COLORS[prvColors[1]];
+            slots[i + 6].style.background = COLORS[prvColors[2]];
+        } else if ( i == 1 ) {
+            rndNum = Math.trunc(Math.random() * COLORS.length);
+            prvColors1.unshift(rndNum);
+            prvColors1.pop;
+            slots[i].style.background = COLORS[prvColors1[0]];
+            slots[i + 3].style.background = COLORS[prvColors1[1]];
+            slots[i + 6].style.background = COLORS[prvColors1[2]];
+        } else {
+            rndNum = Math.trunc(Math.random() * COLORS.length);
+            prvColors2.unshift(rndNum);
+            prvColors2.pop;
+            slots[i].style.background = COLORS[prvColors2[0]];
+            slots[i + 3].style.background = COLORS[prvColors2[1]];
+            slots[i + 6].style.background = COLORS[prvColors2[2]];
+        }
     }
     randomSpins = randomSpins - 1;
     spinBtn.removeEventListener('click', spinSlots);
@@ -24,6 +47,9 @@ function changeSlots () {
         spinBtn.addEventListener('click', spinSlots);
         spinBtn.style.background = 'red';
         spinBtn.style.cursor = 'pointer';
+        console.log(prvColors[1]);
+        console.log(prvColors1[1]);
+        console.log(prvColors2[1]);
     }
 }
 
@@ -31,11 +57,10 @@ function spinSlots () {
         randomSpins = Math.trunc(Math.random() * 15 + 7);
     if( isSpinning == false ) {
         isSpinning = true;
-        auto_slots = setInterval(changeSlots, 100);
+        auto_slots = setInterval(changeSlots, 350);
     } else {
         return;
     }
 }
 
 spinBtn.addEventListener('click', spinSlots)
-
